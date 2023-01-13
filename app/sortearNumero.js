1deec45731885f18a0a2c733bfd1f829933d5f5d
Renato-da-Silva <122237343+Renato-da-Silva@users.noreply.github.com>
2023-01-13 16:09:53 -0300

    Jogo finalizado

---
+++ app/sortearNumero.js
@@ -0,0 +1,16 @@
+const menoValor = 1
+const maiorValor = 1000
+const numeroSecreto = gerarNumeroAleatorio()
+
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor + 1) 
+}
+
+console.log('Número Secreto:',numeroSecreto)
+
+const elementoMenorValor = document.getElementById('menor-valor')
+elementoMenorValor.innerHTML = menoValor
+
+const elementoMaiorValor = document.getElementById('maior-valor')
+elementoMaiorValor.innerHTML = maiorValor
